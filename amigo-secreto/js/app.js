4de436dbd0b0611ed89f21fcefedbca23e73cbb2
Elisariane <elisarianeBS@gmail.com>
2023-10-27 09:58:54 -0300

    Add desafio Amigo Secreto

---
+++ amigo-secreto/js/app.js
@@ -0,0 +1,51 @@
+
+let amigos = [];
+
+function adicionar() {
+    let amigo = document.getElementById('nome-amigo').value;
+    let listaAmigos = document.getElementById('lista-amigos');
+    
+    amigos.push(amigo);
+
+    if (listaAmigos.textContent == ''){
+        listaAmigos.textContent = amigo;
+    } else {
+        listaAmigos.textContent = listaAmigos.textContent + ', ' + amigo;
+    }
+
+    document.getElementById('nome-amigo').value = '';
+}
+
+function sortear() {
+    embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+
+    for (let index = 0; index < amigos.length; index++) {
+
+        if (index == amigos.length -1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[index] + '--> ' + amigos[0] + '<br>';
+        }
+
+        sorteio.innerHTML = sorteio.innerHTML + amigos[index] + '--> ' + amigos[index + 1] + '<br>';
+    }
+
+}
+
+function embaralha(lista) {
+
+    let indice = lista.length
+    
+    while(indice) {
+        const indiceAleatorio = Math.floor(Math.random() * indice--);
+        [lista[indice], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice]];
+    }
+}
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').textContent = '';
+    document.getElementById('lista-sorteio') = ''
+}
+
+
